4fcaccbef83ffc2055e014cb6ceb91fba0009e7c
FeiMa-REC <Mafei.07@foxmail.com>
2023-10-25 18:52:38 +0800

    feat : add ViT class in vit.py

--- ViT/vit.py
+++ ViT/vit.py
@@ -9,72 +9,68 @@ from einops.layers.torch import Rearrange
 def pair(t):
     return t if isinstance(t, tuple) else (t, t)
 
-class PreNorm(nn.Module):
-    def __init__(self, dim, fn):
-        super(PreNorm, self).__init__()
-        self.fn = fn
-        self.norm = nn.LayerNorm(dim)
 
-    def forward(self, x, **kwargs):
-        x = self.norm(x)
-        x = self.fn(x, **kwargs)
-        return x
-
-
-class MLP(nn.Module):
-    def __init__(self, dim, hidden_dim, dropout = 0.):
-        super(MLP, self).__init__()
-        self.net = nn.Sequential(
-            nn.LayerNorm(dim),
-            
-            nn.Linear(dim, hidden_dim),
-            nn.GELU(),
-            nn.Dropout(dropout),
-            
-            nn.Linear(hidden_dim, dim),
-            nn.Dropout(dropout)
-        )
+class ViT(nn.Module):
+    '''
+        args:
+            image_size : 输入图片的大小
+            patch_size : 输入图片被切割成的patch大小，patch的个数必须大于16
+            num_classes : 分类数
+            dim : 线性投影后的维度
+            depth : encoder 的深度
+            heads : 注意力头的个数
+            mlp_dim : 分类层的维度
+            channels : 输入图片的通道数
+            pool : 选用 CLS embedding 或者 mean 平均池化
+    '''
+    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0.):
+        super(ViT, self).__init__()
     
-    def forward(self,x):
-        return self.net(x)
+        image_height, image_width = pair(image_size)
+        patch_height, patch_width = pair(patch_size)
 
+        assert image_height % patch_height == 0 and image_width % patch_width == 0, "Image dimensions must be divisible by the patch size."
 
-class MHA(nn.Module):
-    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
-        super(MHA, self).__init__()
-        inner_dim = heads * dim_head
-        project_out = not (heads == 1 and dim_head == dim)
+        num_patchs = (image_height // patch_height) * (image_width // patch_width)
+        patch_dim = channels * patch_height * patch_width
+        assert pool in {'cls', 'mean'}, "pool type must be either cls or mean."
 
-        self.heads = heads
-        self.scale = dim_head ** -0.5
-        
-        self.norm = nn.LayerNorm(dim)
-        
-        self.attend = nn.Softmax(dim = -1)
-        self.dropout = nn.Dropout(dropout)
+        self.to_patch_embedding = nn.Sequential(
+            Rearrange('b c (h, p1) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
+            nn.LayerNorm(patch_dim),
+            nn.Linear(patch_dim, dim),
+            nn.LayerNorm(dim)
+        )
 
-        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+        self.pos_embedding = nn.Parameter(torch.rand(1, num_patchs + 1, dim))
+        self.cls_token = nn.Parameter(torch.rand(1, 1, dim))
+        self.dropout = nn.Dropout(emb_dropout)
 
-        self.to_out = nn.Sequential(
-            nn.Linear(inner_dim, dim),
-            nn.Dropout(dropout)
-        )if project_out else nn.Identity()
+        self.transformer = ...
 
-    def forward(self, x):
-        b, n, _, h = *x.shape, self.heads
+        self.pool = pool
+        self.to_latent = nn.Identity()
 
-        qkv = self.to_qkv(x).chunk(3, dim = -1)
-        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
+        self.mlp_head = nn.Sequential(
+            nn.Linear(dim, num_classes*2),
+            nn.GELU(),
+            nn.Linear(num_classes*2, num_classes)
+        )
 
-        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+    def forward(self, img):
+        x = self.to_patch_embedding(img)
+        b, n, _ =  x.shape
 
-        attn = self.attend(dots)
-        attn = self.dropout(attn)
+        cls_token = repeat(self.cls_token, '1 1 d -> b, 1, d', b = b)
+        x = torch.cat((cls_token, x), dim=1)
+        x += self.pos_embedding[:, :(n + 1)]
+        x = self.dropout(x)
 
-        out = torch.matmul(attn, v)
-        out = rearrange(out, 'b h n d -> b n (h d)')
-        
-        return self.to_out(out)
+        x = self.transformer(x)
+
+        x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
+        x = self.to_latent(x)
+        return self.mlp_head(x)
 
 if __name__ == "__main__":
     ...
