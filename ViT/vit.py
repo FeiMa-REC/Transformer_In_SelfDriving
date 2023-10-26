36acc78fc0ebec6e2a9b23ae1e3e9219c12e903b
FeiMa-REC <Mafei.07@foxmail.com>
2023-10-26 20:53:03 +0800

    feat : finish vit.py

--- ViT/vit.py
+++ ViT/vit.py
@@ -10,6 +10,81 @@ def pair(t):
     return t if isinstance(t, tuple) else (t, t)
 
 
+class FeedForward(nn.Module):
+    def __init__(self, dim, mlp_dim, dropout = 0.):
+        super(FeedForward, self).__init__()
+        self.layers = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, mlp_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(mlp_dim, dim),
+            nn.Dropout(dropout)
+        )
+        
+    def forward(self, x):
+        return self.layers(x)
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads, dim_head, dropout = 0.):
+        super(Attention, self).__init__()
+        inner_dim = dim_head * heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.norm = nn.LayerNorm(dim)
+
+        self.attend = nn.Softmax(dim = -1)
+        self.dropout = nn.Dropout(dropout)
+
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+        
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x):
+        x = self.norm(x)
+
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
+
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+
+        attn = self.attend(dots)
+        attn = self.dropout(attn)
+
+        out = torch.matmul(attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
+
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+        super(Transformer, self).__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.layers = nn.ModuleList([])
+
+        for _ in range(depth):
+            self.layers.append(
+                nn.ModuleList([
+                    Attention(dim, heads, dim_head, dropout),
+                    FeedForward(dim, mlp_dim, dropout)
+                ])
+            )
+
+    def forward(self, x):
+        for atten, ff in self.layers:
+            x = atten(x) + x
+            x = ff(x) + x
+
+        return self.norm(x)
+
+
 class ViT(nn.Module):
     '''
         args:
@@ -36,7 +111,7 @@ class ViT(nn.Module):
         assert pool in {'cls', 'mean'}, "pool type must be either cls or mean."
 
         self.to_patch_embedding = nn.Sequential(
-            Rearrange('b c (h, p1) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
             nn.LayerNorm(patch_dim),
             nn.Linear(patch_dim, dim),
             nn.LayerNorm(dim)
@@ -46,7 +121,7 @@ class ViT(nn.Module):
         self.cls_token = nn.Parameter(torch.rand(1, 1, dim))
         self.dropout = nn.Dropout(emb_dropout)
 
-        self.transformer = ...
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
 
         self.pool = pool
         self.to_latent = nn.Identity()
@@ -61,7 +136,7 @@ class ViT(nn.Module):
         x = self.to_patch_embedding(img)
         b, n, _ =  x.shape
 
-        cls_token = repeat(self.cls_token, '1 1 d -> b, 1, d', b = b)
+        cls_token = repeat(self.cls_token, '1 1 d -> b 1 d', b = b)
         x = torch.cat((cls_token, x), dim=1)
         x += self.pos_embedding[:, :(n + 1)]
         x = self.dropout(x)
@@ -72,5 +147,21 @@ class ViT(nn.Module):
         x = self.to_latent(x)
         return self.mlp_head(x)
 
+
 if __name__ == "__main__":
-    ...
+    model = ViT(
+        image_size=256,
+        patch_size=32,
+        num_classes=1000,
+        dim=1024,
+        depth=6,
+        heads=16,
+        mlp_dim=2048,
+        dropout=0.1,
+        emb_dropout=0.1
+    )
+
+    img = torch.randn(1, 3, 256, 256)
+
+    preds = model(img)
+    print(preds.shape)
